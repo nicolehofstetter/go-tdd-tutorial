f8f9f7c0d99052a1bf86e8daa9e530587fd4d25e
Nicole Hofstetter <nicole.hofstetter@audi.de>
2022-09-06 12:01:09 +0200

    Adds empty string handling to function

--- main/hello.go
+++ main/hello.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(Hello(""))
-}
+const englishHelloPrefix = "Hello "
 
 func Hello(name string) string {
-	return "Hello " + name + "!"
+
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello(""))
 }
--- main/hello_test.go
+++ main/hello_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Tommy")
-	want := "Hello Tommy!"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Tommy")
+		want := "Hello Tommy"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello World' when empty string is supplied", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
